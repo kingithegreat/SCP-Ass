6be568b810a170fae8b32a759784aad241da1635
kingithegreat <139932842+kingithegreat@users.noreply.github.com>
2024-03-20 11:37:53 +1300

    uploading beta SCP assignment
    all files

---
+++ SCP/scripts.js
@@ -0,0 +1,7 @@
+// let active = document.querySelectorAll(".accordion-div.accordion.active");
+// for (let j = 0; j < active.length; j++) {
+//     active[j].classList.remove("active");
+//     active[j].nextElementSibling.style.maxHeight = null; // Set to null or 0px
+// }
+function showAlert() {
+    alert("YOU ARE NOT AUTHORISED TO VEIW THIS CONTENT"); }
